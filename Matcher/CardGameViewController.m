ff5bf26b41a13e413d911dcfad8fdc523f5c3bb9
Cliff <cliff@apportable.com>
2014-02-27 15:08:34 -0800

    testing to see if I'm cliff

--- Matcher/CardGameViewController.m
+++ Matcher/CardGameViewController.m
@@ -14,16 +14,11 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+- (IBAction)touchCardButton:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *cardImage = [UIImage imageNamed:@"cardback"];
+    [sender setBackg]
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
